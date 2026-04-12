f249c1bca338a9d50d1fb90ba497d190059d2ada
sami <saminsx@gail.com>
2026-04-12 11:23:25 +0300

    Initial python project commit

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app=FastAPI()
+
+@app.get("/")
+def read_index():
+    return {"Hello":"World"}
+
+@app.get("/api/v1/hello-word/")
+def read_hello_word():
+    return {"what":"road", "where":"kubernetes","version":"v1"} 
